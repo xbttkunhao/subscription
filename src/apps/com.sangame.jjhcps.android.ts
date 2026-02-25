ac9648fc491c2c4f3c33c0af6ea584dddfd93c07
xbtt <xbttkunhao@163.com>
2026-02-25 22:46:20 +0800

    接上一个

--- src/apps/com.sangame.jjhcps.android.ts
+++ src/apps/com.sangame.jjhcps.android.ts
@@ -20,7 +20,7 @@ export default defineGkdApp({
     },
     {
       key: 2,
-      name: '首页右下角广告',
+      name: '局部广告-首页右下角广告',
       fastQuery: true,
       actionMaximum: 1,
       resetMatch: 'app',
